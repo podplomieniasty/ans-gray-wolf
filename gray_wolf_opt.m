9d495c482cd2bcab20220870556de510bf159dc1
Bartosz Burzec <burzec.dev@gmail.com>
2023-04-07 14:44:43 +0200

    utworzono gray_wolf_opt

---
+++ gray_wolf_opt.m
@@ -0,0 +1,133 @@
+function [alpha_score, alpha_position, convergence_plot] = gray_wolf_opt(f, dim, min_x, max_x,  pack_size, max_iter)
+    
+    %   ALGORYTM SZAREGO WILKA, v.1
+    %   
+    % 
+    %   
+    % parametry  
+    %-------------------------------------------------------------
+    % f          ->  funkcja celu oraz funkcja jakości wilka
+    % dim        ->  liczba wymiarów oraz argumentów funkcji  celu
+    % min_x      ->  dolna granica
+    % max_x      ->  górna granica
+    % pack_size  ->  liczebność watahy
+    % max_iter   ->  maksymalna liczba iteracji
+    % 
+    % 
+    % zwraca 
+    %-------------------------------------------------------------
+    % alpha_score      ->  optymalny wynik algorytmu 
+    % alpha_position   ->  optymalne pozycje wilka
+    % convergence_plot ->  dane do wykresu konwergencji
+
+
+    % inicjalizacja pozycji startowych oraz wyników
+    % ---------------------------------------------
+    % inf   -> w celach optymalizacyjnych
+    % zeros -> sam wilk nie został wybrany, zostanie wybrany w trakcie
+    %          algorytmu
+
+    alpha_score = inf;
+    alpha_position = zeros(1, dim);
+
+    beta_score = inf;
+    beta_position = zeros(1, dim);
+
+    delta_score = inf;
+    delta_position = zeros(1, dim);
+
+    % inicjalizacja wynikowego wykresu konwergencji
+    % cokolwiek to znaczy lol
+    convergence_plot = zeros(1, max_iter);
+
+    % inicjalizacja pozycji startowych wszystkich wilkow
+    bounds_count = size(max_x, 2);
+    
+    % jeśli jest tylko jedna granica dla wszystkich zmiennych
+    % i jeśli min_x i max_x są pojedynczymi numerami
+    if bounds_count == 1
+        positions = rand(pack_size, dim) .* (max_x - min_x) + min_x; 
+    end
+
+    % jeśli zmienne mają różne granice
+    if bounds_count > 1
+        for i=1:dim
+            max_i = max_x(i);
+            min_i = min_x(i);
+            positions = rand(pack_size, 1) .* (max_i - min_i) + min_i;
+        end
+    end
+
+    % główna pętla licząca 
+    iter = 0;
+    while iter < max_iter
+
+        a = 2 * (1 - (iter/max_iter)); % liniowo zmniejsza się do z 2 do 0
+
+        % szacowanie poziomu sprawności wszystkich wilków,
+        % wybór nowych wilków alfa, beta i delta
+        % oraz aktualizacja ich pozycji
+        for i = 1:size(positions,1)   
+    
+            Flag4ub=positions(i,:)>max_x;  
+            Flag4lb=positions(i,:)<min_x;
+            positions(i,:)=(positions(i,:).*(~(Flag4ub+Flag4lb)))+max_x.*Flag4ub+min_x.*Flag4lb;         
+
+            fitness = f(positions(i, :));
+
+            if fitness < alpha_score
+                alpha_score = fitness;
+                alpha_position = positions(i, :);
+            end
+
+            if fitness > alpha_score && fitness < beta_score
+                beta_score = fitness;
+                beta_position = positions(i, :);
+            end
+
+            if fitness > alpha_score && fitness > beta_score && fitness < delta_score
+                delta_score = fitness;
+                delta_position = positions(i, :);
+            end            
+        end
+        
+
+        for i=1:size(positions, 1)
+            for j=1:size(positions, 2)
+
+                [X1] = get_new_position(a, alpha_position(j), positions(i, j));
+                [X2] = get_new_position(a, beta_position(j), positions(i, j));
+                [X3] = get_new_position(a, delta_position(j), positions(i, j));
+
+                positions(i, j) = (X1 + X2 + X3) / 3;
+            end
+        end
+        iter = iter + 1;
+        convergence_plot(iter) = alpha_score;
+
+    end % while END
+
+    function [X] = get_new_position(a, wolf_pos, glob_pos)
+    
+    % funkcja do zwrocenia pozycji jednego z glownych wilkow.
+    % mozna bylo to zrobic po prostu w kodzie zamiast w funkcji.
+    % nie lubie powtarzalnosci wiec trudno hehe
+    %   
+    % parametry
+    % -----------------------------------------------------------------
+    % a         ->  współczynnik liniowy podany wyżej
+    % wolf_pos  ->  pozycja danego wilka z macierzy w danej iteracji, 
+    %               np. alfa(j)
+    % glob_pos  ->  pozycja wilka w macierzy(x, y), np. positions(i, j)
+
+        r1 = rand();
+        r2 = rand();
+
+        A = 2 * a * r1 - a;
+        C = 2 * r2;
+        
+        D_wolf = abs(C * wolf_pos - glob_pos);
+        X = wolf_pos - A * D_wolf;
+    end % get_new_position END
+
+end % gray_wolf_opt END
--- main.m
+++ main.m
@@ -1,1 +1,69 @@
-disp('Hello world!')
+%
+%   Aktualnie w tej funkcji znajduje sie wszystko
+%   żeby zobaczyć jak kod działa i czy działa tak
+%   jak autor zamierzył
+%   status: działa
+%
+
+
+clc
+clear
+
+% DANE OD TWORCY
+fun = @F7; % funkcja celu do optymalizacji
+agents_num = 30;
+iterations = 1000;
+lb=-600; 
+ub=600;
+dim=100;
+[best_score, best_pos, convergence_plot] = gray_wolf_opt(fun, dim, lb, ub, agents_num, iterations);
+display(['best score:', num2str(best_score)]);
+display(['best pos:', num2str(best_pos)]);
+
+% od tworcow
+% zgadza sie, ukradlem
+% ale tylko glupi by nie skorzystal
+% trzeba bylo pilnowac 
+
+%Draw objective space
+figure('Position',[500 500 660 290])
+%Draw search space
+subplot(1,2,1);
+x = -10:0.1:10
+y = x;
+l = length(x)
+for i = 1:l
+    for j=1:l
+        z(i,j) = fun([x(i), y(j)]);
+    end
+end
+surfc(x,y,z, 'LineStyle', 'none');
+title('Parameter space')
+xlabel('x_1');
+ylabel('x_2');
+zlabel(['Funkszjon','( x_1 , x_2 )'])
+
+subplot(1,2,2);
+semilogy(convergence_plot,'Color','r')
+title('Objective space')
+xlabel('Iteration');
+ylabel('Best score obtained so far');
+
+axis tight
+grid on
+box on
+legend('GWO')
+
+
+% TESTOWA FUNKCJA Z PRZYKLADU OD TWORCOW
+function o = F7(x)
+    F2 = 1;
+    one = 0;
+    two = 1;
+    for i = 1:length(x)
+        one = one + (x(i)^2/4000);
+        two = two * (cos(x(i)/sqrt(i)));
+    end
+    o = F2 + one + two;
+end
+
