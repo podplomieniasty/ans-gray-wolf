a9150ac73fbb6c6a6942c4a7794c72eb1c2fd4bf
Bartosz Burzec <burzec.dev@gmail.com>
2023-05-30 11:54:04 +0200

    fixes-4.0

--- gray_wolf_opt.m
+++ gray_wolf_opt.m
@@ -118,8 +118,9 @@ function [alpha_score, alpha_position, convergence_plot] = gray_wolf_opt(f, dim,
 
     end % while END
 
-    alpha_position = transpose(round(alpha_position));
+    alpha_position = transpose(ceil(alpha_position));
     alpha_score = optFactor * alpha_score;
+    convergence_plot = convergence_plot * optFactor;
 
     function [X] = get_new_position(a, wolf_pos, glob_pos)
     
